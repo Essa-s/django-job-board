a067611579a8cce41c199c79dc238087efae8b8f
Essa-s <essa4a@gmail.com>
2023-12-28 04:07:04 +0200

    contact & mailsend

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from .models import Info
 # Create your views here.
-
+from django.core.mail import send_mail
+from django.conf import settings
 def send_message(request):
-    pass
+    info = Info.objects.first()
+    
+    if request.method == "POST":
+        subject = request.POST['subject']
+        email = request.POST['email']
+        message = request.POST['message']
+    send_mail(
+        subject,
+        message +'   '+ email,
+        email,
+        [settings.EMAIL_HOST_USER],
+        fail_silently=False,
+        )
+    return render(request, 'contact/contact.html',{'info':info})
