95b4d5eafc1f43383096e39d42500337ee10ba7e
Essa-s <essa4a@gmail.com>
2023-12-28 02:14:40 +0200

    nav bar links

--- job/views.py
+++ job/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from .models import Job
 from django.core.paginator import Paginator
 from .forms import ApplyForm, JobForm
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def job_list(request):
@@ -27,7 +28,7 @@ def job_detail(request, slug):
         form = ApplyForm()
     context = {'job': job_detail, 'form': form}
     return render(request, "job/job_detail.html", context)
-
+@login_required
 def add_job (request):
     if request.method =="POST":
         form = JobForm(request.POST, request.FILES)
